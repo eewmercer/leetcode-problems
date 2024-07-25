7f1cfd978b24e43d9d90c363889693d2a605c493
Emmaline Mercer <eewmercer@gmail.com>
2024-07-25 15:02:42 -0400

    Complete mergeAlternately method

--- merge-strings-alternately/MergeAlternateStrings.java
+++ merge-strings-alternately/MergeAlternateStrings.java
@@ -1,22 +1,25 @@
 class MergeAlternateStrings {
     public String mergeAlternately(String word1, String word2) {
-        String empty = "";
-        String mergeWords = "";
+        String result = ""; //could use a StringBuilder here instead too
 
         char[] word1Array = word1.toCharArray();
+        System.out.println(word1Array.length);
         char[] word2Array = word2.toCharArray();
+        System.out.println(word1Array.length);
 
         int j = 0;
-        for (int i = 0; i < word1Array.length; i++) {
-            mergeWords += String.valueOf(word1Array[i]);
-            mergeWords += String.valueOf(word2Array[j]);
-            j++;
+        for (int i = 0; i < (word1Array.length + word2Array.length); i++) { 
+            if (i < word1Array.length) {
+                result += String.valueOf(word1Array[i]);
+            }
 
-            // mergeWords += String.valueOf(word2Array[i]);
-            // mergeWords + String.valueOf(word2Array[i]);
+            if (j < word2Array.length) {
+                result += String.valueOf(word2Array[j]);
+                j++;
+            }
         }
 
-
-        return mergeWords;
+        System.out.println(result);
+        return result;
     }
 }
---
+++ run.java
@@ -0,0 +1,6 @@
+class run {
+    public static void main(String[] args) {
+        MergeAlternateStrings merge = new MergeAlternateStrings();
+        merge.mergeAlternately("abc", "pqr");
+    }
+}
